033f54bfb99589e7560edee335ca67b2a865719a
Hafeez <syedhafeez957@gmail.com>
2025-12-09 12:44:50 +0530

    implementing searching feature in products page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const productsMain = document.querySelector('.products-main');
 const categories = document.querySelectorAll('.product-categories a');
 const electronicsProducts = document.querySelector('.electronics-products');
+
 console.log(electronicsProducts);
 
 
@@ -24,17 +25,23 @@ productsMain.innerHTML = `<div class="loader"></div>`;
                     <img src="${image}" alt="${title} class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title">${title}</p>
+                         <p class="card-title">$${price}</p>
                         <!--<p class="card-text">${description}</p>-->
-                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                        <a href="#" class="btn btn-primary" onclick="addToCart(${id})">Add to Cart</a>
                     </div>
                 </div>
             </div>
         `
     }
+    // all products category
+    categories[0].addEventListener("click", () => {
+        window.navigation.reload();
+    })
+
     productsMain.innerHTML = output;
     categories.forEach(option => {
         option.onclick = (e) => {
-            // e.preventdefault();
+            // e.preventDefault();
             let selected_category = e.target.innerText.toLowerCase();
             let filtered_output = "";
             for (const product of api_data) {
@@ -47,16 +54,46 @@ productsMain.innerHTML = `<div class="loader"></div>`;
                                         <img src="${product.image}" alt="${product.title} class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <p class="card-title">${product.title}</p>
+                                             <p class="card-title">$${product.price}</p>
                                             <a href="#" class="btn btn-primary">Add to Cart</a>
                                         </div>
                                     </div>
                                 </div>
         
                              `
-                            }
-                        }
-                        productsMain.innerHTML = filtered_output;
+                }
+            }
+            productsMain.innerHTML = filtered_output;
         }
     })
+
+    // search products
+
+    let search = document.getElementById('search-products');
+    search.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let searchProduct = e.target.search.value;
+        let searchFilter = "";
+        for (const product of api_data) {
+            let { title, image, price } = product;
+            if (title.toLowerCase().includes(searchProduct.toLowerCase().trim())) {
+                searchFilter += `
+                                <div class="card col-3">
+                                        <img src="${image}" alt="${title} class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <p class="card-title">${title}</p>
+                                            <p class="card-title">$${price}</p>
+                                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                                        </div>
+                                    </div>
+                                </div>
+                             `
+            }
+        }
+        productsMain.innerHTML = searchFilter || "<h3>No Product found";
+    })
 })()
+
+
+
 
